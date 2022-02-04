c80a2bbdc2d567c603bd26f79ba2d13123b3f5ed
Paul Grudnitski <paul.grudnitski@pearson.com>
2022-02-04 09:42:47 -0800

    Add raised card style

--- src/components/qti/QtiAssessmentItem.vue
+++ src/components/qti/QtiAssessmentItem.vue
@@ -2054,4 +2054,9 @@ hr {
   background-color: var(--item-card-panel-bgc);
   border-top: 1px solid var(--item-card-panel-bc);
 }
+
+.qti3-player-item-card-raised {
+  border-radius: 2px;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.12),0 1px 5px 0 rgba(0,0,0,.2);
+}
 </style>
