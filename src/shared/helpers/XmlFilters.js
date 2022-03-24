853f7db0dccc532467948b83d78210352b55b48f
Paul Grudnitski <paul.grudnitski@pearson.com>
2022-03-24 16:08:57 -0700

    Add XmlFilter to cleaning closed track and source elements

--- src/shared/helpers/XmlFilters.js
+++ src/shared/helpers/XmlFilters.js
@@ -1,8 +1,13 @@
 export class XmlFilters {
+
+  #rxTrackSource = null
+
   /**
    * @description Helper Class for transforming the QTI XML
    */
   constructor() {
+    // Regex matching closed 'track' and 'source' elements
+    this.#rxTrackSource = new RegExp(/<(track|source)[^>]+?\/>/g)
   }
 
   /**
@@ -23,9 +28,21 @@ export class XmlFilters {
   /**
    * @description Transform an audio element to an amp-audio element which loads
    * the custom amp-up.io audio player instead of the the default html5 audio player.
+   * @NOTE UPDATE: transform any closed <source> or <track> elements.
    */
   filterAudio (xml) {
-    return xml.replaceAll('<audio ','<amp-audio ').replaceAll('</audio>','</amp-audio>')
+    return this.filterTrackSource(xml)
+    //return xml.replaceAll('<audio ','<amp-audio ').replaceAll('</audio>','</amp-audio>')
   }
 
+  /**
+   * @description Transform closed <source /> and <track /> elements into
+   * required HTML5 **open** elements.  Without this, <video> and <audio> elements
+   * do not work on Safari.
+   */
+  filterTrackSource (xml) {
+    return xml.replace(this.#rxTrackSource, (match) => {
+      return match.replace(/\/>/g, '>')
+    })
+  }
 }
