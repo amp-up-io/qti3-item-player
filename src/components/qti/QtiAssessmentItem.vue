3ebe288ac7b7a0e1e9d5d76079fc911bef0a8fc1
Paul Grudnitski <paul.grudnitski@pearson.com>
2023-01-03 11:54:54 -0800

    Provisional css support for vertical-rl
    - plus, add text-orientation: upright and text-transform: full-width

--- src/components/qti/QtiAssessmentItem.vue
+++ src/components/qti/QtiAssessmentItem.vue
@@ -2441,4 +2441,17 @@ code {
   border-radius: 5px;
   box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.12),0 1px 5px 0 rgba(0,0,0,.2);
 }
+
+.qti3-player-template-vertical-rl {
+  writing-mode: vertical-rl;
+  float: right;
+}
+
+.qti3-player-text-orientation-upright {
+  text-orientation: upright;
+}
+
+.qti3-player-text-transform-fullwidth {
+  text-transform: full-width;
+}
 </style>
