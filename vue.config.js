c23b6a1813bb523ea604763b1cf919b071c1561f
paulgrudnitski <paul.grudnitski@gmail.com>
2024-09-13 13:07:58 -0700

    Update vue.config.js - Fix build:npm 'from' argument error

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   runtimeCompiler: true,
 
+  // Fix for https://github.com/vuejs/vue-cli/issues/7291
+  // "The 'from' argument must be of type string"
+  parallel: false,
+
   configureWebpack: {
     plugins: [
       new CopyWebpackPlugin([
